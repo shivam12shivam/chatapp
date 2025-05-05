20903acc4e4ed69a8aaa7a470412b5022b53cfce
shivam <shivam198237@gmail.com>
2025-05-05 22:50:33 +0530

    backend url

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://chatapp-six-lilac.vercel.app/",
+        origin: "https://chatapp-six-lilac.vercel.app",
         methods: ["GET", "POST"],
     },
 });
