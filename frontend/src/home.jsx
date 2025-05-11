3f9859c89738071e37d99e76aa00803a2b829f8b
shivam <shivam198237@gmail.com>
2025-05-11 15:20:02 +0530

    resposive

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -6,7 +6,7 @@ import Logout from './left/logout'
 function Home() {
     return (
         <div className='flex w-screen h-screen bg-[rgb(92,80,130)]'>
-            <div className=' bg-[rgb(125,108,181)] text-white items-center pb-2 flex flex-col justify-end w-12'>
+            <div className=' bg-[rgb(125,108,181)] text-white items-center pb-2 flex flex-col justify-end w-4 md:w-12'>
                 <Logout />
             </div>
             <div className='h-screen bg-[rgb(92,80,130)] w-96 '>
